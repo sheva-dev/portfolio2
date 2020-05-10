771bb19b59ec42553be5448a68d2bd213deb0ac3
Amin <hamadagood111@gmail.com>
2020-05-10 20:58:33 +0200

    Update global style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import Nav from '../Nav/Nav';
 import WelcomeSection from '../WelcomeSection/WelcomeSection';
+import MyWorkSection from '../MyWorkSection/MyWorkSection';
 
 const App = () => {
     return (
         <div>
+            <Nav />
             <WelcomeSection />
+            <MyWorkSection />
         </div>
     );
 }
