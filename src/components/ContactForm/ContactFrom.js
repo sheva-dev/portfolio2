2bd1ddd29366995eb4f114fec568b67636e06dbf
Amin <hamadagood111@gmail.com>
2020-10-19 13:54:59 +0200

    Add emailJs && form validation

--- src/components/ContactForm/ContactFrom.js
+++ src/components/ContactForm/ContactFrom.js
@@ -1,40 +1,63 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import './ContactFrom.css';
+import { useForm } from 'react-hook-form';
+import { init, sendForm } from 'emailjs-com';
+init("user_YDEuIcnG28EVIxhyk1PwB");
 
 const ContactFrom = () => {
-    const { register, handleSubmit, errors, setValue } = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const [confMsg, setConfMsg] = useState(false);
     const [name, setName] = useState('');
 
     const onSubmit = (data) => {
-        setConfMsg(true);
-        setName(data.name)
-        setValue('name', '');
-        setValue('email', '');
-        setValue('message', '');
+        const form = document.querySelector('#contact-form');
+
+        sendForm('gmail', 'portofolio-template', 'contact-form')
+            .then(res => {
+                setConfMsg(true);
+                setName(data.name);
+                console.log('SUCCESS!', res.status, res.text)
+                form.reset();
+            }, error => {
+                console.log('FAILED...', error)
+            });
     }
 
     return (
         <>
             <div className="container">
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(onSubmit)} className='contact-form' id='contact-form'>
                     <div className="row">
                         <div className="col-75">
-                            <input type="text" id="name" name="name" placeholder="Name" ref={register({ required: true })} />
-                            <p style={{ marginBottom: "2px" }}>{errors.message && 'Name is required.'}</p>
+                            <input type="text" id="name" name="name" placeholder="Name"
+                                ref={register({
+                                    required: true,
+                                    pattern: /^[A-Za-z]+$/i
+                                })} />
+                            <p> {errors.name && 'Name is required.'}</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-75">
-                            <input type="email" id="name" name="email" placeholder="Email" ref={register({ required: true })} />
-                            <p style={{ marginBottom: "2px" }}>{errors.message && 'email is required.'}</p>
+                            <input type="email" id="name" name="email" placeholder="Email" ref={register({
+                                required: "Enter your e-mail",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "Enter a valid e-mail address",
+                                }
+                            })}
+                            />
+                            {errors.email && <p className="error">{errors.email.message}</p>}
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-75">
-                            <textarea id="subject" name="message" placeholder="Your Message" rows="5" ref={register({ required: true })}></textarea>
-                            <p style={{ marginBottom: "2px" }}>{errors.message && 'Message is required.'}</p>
+                            <textarea id="subject" name="message" placeholder="Your Message" rows="5"
+                                ref={register({
+                                    required: true,
+                                    pattern: /^[A-Za-z]+$/i
+                                })}></textarea>
+                            <p>{errors.message && 'Message is required.'}</p>
                         </div>
                     </div>
                     <div className="row-button">
